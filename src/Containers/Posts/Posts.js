f74d465d050afd401a921772e7d4f785bd50ca47
Steve Hamm <steve@brightenapparel.com>
2020-05-10 09:18:54 -0300

    Fixed retrieval of posts and added scroll package

--- src/Containers/Posts/Posts.js
+++ src/Containers/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from '../../axios';
 import { Link } from 'react-router-dom';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import Post from '../../components/Post/Post';
 import Aux from '../../hoc/Aux/Aux';
@@ -12,21 +13,26 @@ class Posts extends Component {
   state = {
     posts: [],
     loading: false,
-    render: false
+    error: false
   }
 
-  componentDidMount () {
+  componentDidMount () { 
+    this.retrieveFourPosts();
+  }
+
+  retrieveFourPosts = () => {
     this.setState({loading: true});
 
     axios.get('/article_group/article.json')
       .then(response => {
-        const posts = response.data;
-        const updatedPosts = Object.values(posts);
-        this.setState({posts: updatedPosts, loading: false});
-    })
-    .catch(error => {
-      this.setState({error: true, loading: false});
-    });
+        const result = Object.keys(response.data).map((k) => response.data[k]);
+        const newestPosts = result.sort((a, b) => b.date_edited - a.date_edited);
+        const posts = newestPosts.slice(0, 4);     
+        this.setState({posts: posts, loading: false});
+      })
+      .catch(error => {
+        this.setState({error: true, loading: false});
+      });
   }
 
   postSelectedHandler = ( id ) => {
