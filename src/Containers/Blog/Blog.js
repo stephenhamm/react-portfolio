073894d63d99f75dda8928315892390f4a003553
Steve Hamm <steve@brightenapparel.com>
2020-05-12 17:19:51 -0300

    Added footer links and code cleanup.

--- src/Containers/Blog/Blog.js
+++ src/Containers/Blog/Blog.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 import Posts from '../Blog/Posts/Posts';
+import classes from './Blog.module.css';
 
 class Blog extends Component {
   render () {
     return (
       <div>
         <h1>BLOG POSTS</h1>
-        <Posts/>
+        <div className={classes.Posts}>
+          <Posts/>
+        </div>
       </div>
     );
   }
