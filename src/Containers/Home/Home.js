9c8d7b31922518f6aef0e70c5320d17f9e9aad4b
Steve Hamm <steve@brightenapparel.com>
2020-05-17 12:06:44 -0300

    restyled navigation and padding.

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -4,10 +4,10 @@ import Fade from 'react-reveal';
 
 import Posts from '../Blog/Posts/Posts';
 import Section from '../Section/Section';
-import Coding from '../../assets/images/coding.png';
+import Coding from '../../assets/images/coding.jpg';
 import Photography from '../../assets/images/photography.jpeg';
 import ScreePrinting from '../../assets/images/screenprinting.jpg';
-import GraphicDesign from '../../assets/images/graphicdesign.jpg';
+import GraphicDesign from '../../assets/images/graphicdesign.png';
 import classes from './Home.module.css';
 
 const Home = () => (
@@ -15,7 +15,9 @@ const Home = () => (
     <div className={classes.Image}>
       <div className={classes.Overlay}>
         <Fade clear>
-          <img src={require('../../assets/images/profile.jpg')} alt="steve" />
+          <div>
+            <img src={require('../../assets/images/profile.jpg')} alt="steve" />
+          </div>
           <div className={classes.Details}>
             <span className={classes.Name}>STEVE HAMM</span>
             <span className={classes.Title}>SOFTWARE DEVELOPER</span>
@@ -25,10 +27,10 @@ const Home = () => (
       </div>
     </div>
     <div className={classes.Home}>
-    <h1>SKILLS</h1>
-    <div className={classes.Sections}>
+      <h1>SKILLS</h1>
+      <div className={classes.Sections}>
         <Link to={"/"} className={classes.SectionLink}>
-          <Section title="CODING" image={Coding} />
+          <Section title="WEB DEVELOPMENT" image={Coding} />
         </Link>
         <Link to={"/"} className={classes.SectionLink}>
           <Section title="GRAPHIC DESIGN" image={GraphicDesign} />
